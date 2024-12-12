5999402fb79c74a06562ad3285b9e9412d345d73
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-12-12 13:30:11 +0100

    fix: style

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 export default function MovieCard({ movie }) {
     return (
         <>
-            <div className="card p-3">
+            <div className="card p-3" style={{ minHeight: "280px" }}>
                 <h3 className="mb-3">{movie.title}</h3>
                 <span className="mb-3"><strong>Diretto da: </strong>{movie.director}</span>
-                <p className="mb-4">{movie.abstract}</p>
+                <div style={{ minHeight: "100px" }}>
+                    <p className="mb-4">{movie.abstract}</p>
+                </div>
                 <Link to={`movies/${movie.id}`} className="btn btn-dark btn-md">
                     <span>Vai alle recensioni!</span>
                 </Link>
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,7 +1,7 @@
 export default function ReviewCard({ review }) {
     return (
         <>
-            <div className="card p-3">
+            <div className="card p-3" style={{ minHeight: "200px" }}>
                 <h4 className="mb-3"><strong>Recensione di: </strong>{review.name}</h4>
                 <p><strong>Cosa ne pensa: </strong>{review.text}</p>
                 <strong>Voto: {review.vote}/5 </strong>
