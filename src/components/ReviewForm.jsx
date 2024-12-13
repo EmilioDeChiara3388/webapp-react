eb60e9dcedcd67682431c2a577ca05b9188f4e89
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-12-13 17:51:02 +0100

    add: error messagge if the fields are not full and min length

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -6,29 +6,37 @@ export default function ReviewForm({ movie_id }) {
     const [review, setReview] = useState("")
     const [vote, setVote] = useState(0)
     const [okMessage, setOkMessage] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
 
     function HandleForm(e) {
         e.preventDefault()
 
-        const formData = {
-            name: userName,
-            text: review,
-            vote
-        }
+        if (userName.length < 3 || review.length < 10 || rating == 0) {
+            setErrorMessage('Compila tutti i campi!')
 
-        fetch(`http://localhost:3000/api/movies/${movie_id}/reviews`, {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
+        } else {
+            setErrorMessage("")
+
+            const formData = {
+                name: userName,
+                text: review,
+                vote
             }
-        }).then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.success) {
-                    setOkMessage('Grazie per la tua recensione!')
+
+            fetch(`http://localhost:3000/api/movies/${movie_id}/reviews`, {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    "Content-Type": "application/json"
                 }
-            }).catch(err => console.log(err))
+            }).then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.success) {
+                        setOkMessage('Grazie per la tua recensione!')
+                    }
+                }).catch(err => console.log(err))
+        }
 
         setUserName('')
         setReview('')
@@ -45,18 +53,19 @@ export default function ReviewForm({ movie_id }) {
                 <form onSubmit={HandleForm} className="card mb-3 p-3">
                     <div>
                         <label htmlFor="username">User Name</label>
-                        <input type="text" className="form-control mb-3 mt-2 w-25" name="username" id="username" placeholder="Inserisci il tuo username" value={userName} onChange={(e) => setUserName(e.target.value)} />
+                        <input type="text" className="form-control mb-3 mt-2 w-25" name="username" id="username" placeholder="Inserisci il tuo username" value={userName} onChange={(e) => setUserName(e.target.value)} required />
                     </div>
                     <div>
                         <label htmlFor="review">La tua recensione</label>
-                        <textarea className="form-control mb-3 mt-2 w-50" name="review" id="review" placeholder="Inserisci la tua recensione" value={review} onChange={(e) => setReview(e.target.value)}></textarea>
+                        <textarea className="form-control mb-3 mt-2 w-50" name="review" id="review" placeholder="Inserisci la tua recensione" value={review} onChange={(e) => setReview(e.target.value)} required></textarea>
                     </div>
                     <div>
                         <label htmlFor="vote">Il tuo voto</label>
-                        <input type="text" className="form-control mb-3 mt-2 w-25" name="vote" id="vote" placeholder="Il tuo voto" value={vote} onChange={(e) => setVote(e.target.value)} />
+                        <input type="text" className="form-control mb-3 mt-2 w-25" name="vote" id="vote" placeholder="Il tuo voto" value={vote} onChange={(e) => setVote(e.target.value)} required />
                     </div>
                     <div>
                         <button type="submit" className="btn btn-dark btn-sm">Invia!</button>
+                        {errorMessage && <span className="text-danger"> {errorMessage}</span>}
                     </div>
                 </form>
             </div>
